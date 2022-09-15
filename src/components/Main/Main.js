1b379aa19d276943207efa513bb73f30b1f24e31
Zachary Piontek <fortunefavorsthebrave@protonmail.com>
2022-09-14 18:53:22 -0700

    added styles to main and background image

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -2,14 +2,16 @@ import React, { useState } from 'react';
 
 import Option from '../Options/Option.js';
 import Display from '../Displays/Display.js';
+import background from '../../city-background.png';
+import '../Main/main.css';
 
 export default function Main() {
-  const [head, setHead] = useState('skull-1');
+  const [head, setHead] = useState('skull-3');
   const [body, setBody] = useState('body-1');
-  const [legs, setLegs] = useState('legs-1');
+  const [legs, setLegs] = useState('legs-3');
 
   return (  
-    <main>
+    <main style={{ backgroundImage: `url(${background})` }}>
       <Option setHead={setHead} setBody={setBody} setLegs={setLegs}/>   
       <Display head={head} body={body} legs={legs}/>
     </main>
--- src/components/Options/Option.js
+++ src/components/Options/Option.js
@@ -6,9 +6,9 @@ export default function Option({ setHead, setBody, setLegs }) {
       <div>
         <label>Choose a Head</label>
         <select onChange={(event) => setHead(event.target.value)}>
+          <option value='skull-3'>Head-3</option>
           <option value='skull-1'>Head-1</option>
           <option value='skull-2'>Head-2</option>
-          <option value='skull-3'>Head-3</option>
         </select>
       </div>
       <div>
@@ -22,9 +22,9 @@ export default function Option({ setHead, setBody, setLegs }) {
       <div>
         <label>Choose a Legs</label>
         <select onChange={(event) => setLegs(event.target.value)}>
+          <option value='legs-3'>Legs-3</option>
           <option value='legs-1'>Legs-1</option>
           <option value='legs-2'>Legs-2</option>
-          <option value='legs-3'>Legs-3</option>
         </select>
       </div>
     </div>
