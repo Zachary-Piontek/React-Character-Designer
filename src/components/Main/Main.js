be3d08b95ffc34f1ec0327090321fb60e02b755e
Zachary Piontek <fortunefavorsthebrave@protonmail.com>
2022-09-15 12:24:07 -0700

    added catchphrase component and displays is working

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 
 import Option from '../Options/Option.js';
 import Display from '../Displays/Display.js';
+import List from '../Lists/List.js';
 import background from '../../city-background.png';
 import '../Main/main.css';
 
@@ -9,11 +10,13 @@ export default function Main() {
   const [head, setHead] = useState('skull-3');
   const [body, setBody] = useState('body-1');
   const [legs, setLegs] = useState('legs-3');
+  const [phrase, setPhrase] = useState(['Bones is all I got.']);
 
   return (  
     <main style={{ backgroundImage: `url(${background})` }}>
       <Option setHead={setHead} setBody={setBody} setLegs={setLegs}/>   
-      <Display head={head} body={body} legs={legs}/>
+      <Display head={head} body={body} legs={legs} phrase={phrase}/>
+      <List phrase={phrase} setPhrase={setPhrase}/>
     </main>
   );
 }
